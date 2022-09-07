d3bea00bfbb6a553b060bd4fd403c7fa5fce997e
Mik <rodriguez.mcsol@gmail.com>
2022-09-06 23:29:45 -0300

    Agrego sucursales

--- servicios_clientes/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ServiciosClientesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'servicios_clientes'
--- servicios_clientes/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class servicios_clientes(models.Model):
-    title=models.CharField(max_length=225)
-    content=models.TextField()
-    
